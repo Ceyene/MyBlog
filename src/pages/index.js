d122509e93bb8a9daef08210023a1e28f94ddc49
Ceyene <cyn.jaz.romero@gmail.com>
2021-04-26 21:20:04 -0300

    Querying my blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,23 @@ import { graphql } from "gatsby";
 import React from "react";
 
 const Home = ({ data }) => {
-  console.log(data.site);
+  const siteTitle = data.site.siteMetadata.title;
+  const siteDescription = data.site.siteMetadata.description;
+  const posts = data.allMdx.nodes;
+
   return (
     <div>
       <h1>Front End Dev Adventures!</h1>
+      {posts.map((post) => {
+        const title = post.frontmatter.title || post.fields.slug;
+        return (
+          <div>
+            <h1>{title}</h1>
+            <p>{post.frontmatter.date}</p>
+            <p>{post.frontmatter.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -18,6 +31,17 @@ export const pageQuery = graphql`
       siteMetadata {
         title
         description
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        slug
+        frontmatter {
+          date(formatString: "Do MMMM YYYY")
+          title
+          description
+        }
       }
     }
   }
